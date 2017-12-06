fe1bf70c2745240966db1916b0a9bdeb7aab1a92
Dago31 <dagoa7x@gmail.com>
2017-12-06 01:11:43 -0300

    Add files via upload

--- Tics/intento.py
+++ Tics/intento.py
@@ -10,39 +10,59 @@ cur = conn.cursor()
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 time.sleep(3)
 e = 0
-a = 0
-w = 0
 while True:
     try:
         date = datetime.now()
 
         line1 = ser.readline()
 
-        line1 = line1.decode("utf-8")
-        if line1[1] == "1":
-            sql = """
-            insert into alerta (id, fecha) values ((%s), ('%s'));
-            """%(w, date)
-            cur.execute(sql)
-            conn.commit()
-            w = w +1
+        #line1 = line1.decode("utf-8")
+        if line1[0]=="1":
+            if e == 0:
+                sql = """
+                select fecha from encendido;
+                """
+                cur.execute(sql)
+                enc = cur.fetchall()
+                sql = """
+                select fecha from apagado;
+                """
+                cur.execute(sql)
+                apa = cur.fetchall()
+                print(enc)
+                print(apa)
+                e = 1
+
+            if line1[2] == "1":
+                sql = """
+                insert into alerta ( fecha) values ( ('%s'));
+                """%( date)
+                cur.execute(sql)
+                conn.commit()
+            
+
+            else:
+                if date.minute%2 == 0:
+                    ser.write('5')
+                else:
+                    ser.write('2')
         else:
-
-            if line1[0] == "1":
+            e = 0
+            if line1[1] == "1":
                 sql = """
-                insert into encendido (id, fecha) values ((%s), ('%s'));
-                """%(e, date)
+                insert into encendido (fecha) values (('%s'));
+                """%(date)
                 cur.execute(sql)
                 conn.commit()
-                e = e +1
+              
 
-            if line1[0] == "0":
+            if line1[1] == "0":
                 sql = """
-                insert into apagado (id, fecha) values ((%s), ('%s'));
-                """%(a, date)
+                insert into apagado (fecha) values (('%s'));
+                """%(date)
                 cur.execute(sql)
                 conn.commit()
-                a = a + 1
+               
 
 
         print("l1",line1,date)
